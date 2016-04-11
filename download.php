4350e4851eafeb36514215095b46bb8ca774c8c8
mitchellurgero <mitchellurgero@gmail.com>
2016-04-11 01:20:58 +0000

    Fix small typo in downloads

--- download.php
+++ download.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (isset($_SESSION['username']) && isset($_GET['filename'])) {
-  $file = '/home/pi/uploads/'.$_GET['filename'];
+  $file = './app/scr_up/'.$_GET['filename'];
   header('Content-type: application/octet-stream');
   header('Content-Disposition: attachment; filename="'.$_GET['filename'].'"');
   header('Content-length: ' . filesize($file));
