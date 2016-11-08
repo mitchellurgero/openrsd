dc538cd777a7f9c10b4679014adf0e6dbf2cf059
mitchellurgero <info@urgero.org>
2016-11-08 22:20:34 +0000

    Moved table to different spot on page for better readability.

--- pages/openvpn.php
+++ pages/openvpn.php
@@ -29,18 +29,24 @@ echo '';
     				foreach($log_files as $log){
     					$f = explode("/", $log);
     					$file = end($f);
-    					echo'<tr><td><a href="#" onClick="displayProfile(\''.$log.'\')">'.$log.'</a></td><td><button class="btn btn-sm btn-raised btn-warning" onclick="rProfile(\''.$log.'\')">Revoke Client</button></td><td><a href="dlnd_profile.php?filename='.$file.'" class="btn btn-sm btn-raised btn-info">Download</a></td></tr>';
+    					echo'<tr><td style="vertical-align: middle;"><a href="#" onClick="displayProfile(\''.$log.'\')">'.$log.'</a></td><td><button class="btn btn-sm btn-raised btn-warning" onclick="rProfile(\''.$log.'\')">Revoke Client</button></td><td><a href="dlnd_profile.php?filename='.$file.'" class="btn btn-sm btn-raised btn-info">Download</a></td></tr>';
     				}
     				?>
     			</tbody>
     		</table>
-    	</div>
+    		<table class="table">
+    			<thead>
+    				<th>OpenVPN Client List</th>
+    			</thead>
+			</table><br>
+			<pre><?php echo shell_exec("sudo cat /var/log/openvpn-status.log"); ?></pre>
+	    </div>
 	<div class="col-lg-6">
 		<table class="table">
     			<thead>
     				<th>OpenVPN Profile Status</th>
     			</thead>
-		</table><br>
+		</table>
 	<?php
 	$profile_stats = shell_exec("pivpn list");
 	echo "<pre>".$profile_stats."</pre>";
@@ -49,12 +55,7 @@ echo '';
     </div>
     <div class="row">
 	<div class="col-lg-12">
-		<table class="table">
-    			<thead>
-    				<th>OpenVPN Client List</th>
-    			</thead>
-		</table><br>
-		<pre><?php echo shell_exec("sudo cat /var/log/openvpn-status.log"); ?></pre>
+		
 	</div>
 
     </div>
