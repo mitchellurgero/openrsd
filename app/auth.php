498afbf00f5e6a7139e1b13001a94f4cf29018be
mitchellurgero <info@urgero.org>
2016-11-11 22:33:54 +0000

    Auth logging now enabled for failed attempts only.

--- app/auth.php
+++ app/auth.php
@@ -1,6 +1,13 @@
 <?php
 function auth($username, $password){
-	if(file_exists("app/blocked_ip/".$_SERVER['REMOTE_ADDR'])){
+	$date = date('m-d-Y.h:i:s.a', time());
+	$date2 = date('m-d-Y', time());
+	$dir = getcwd();
+	$ip = $_SERVER['REMOTE_ADDR'];
+	if(!file_exists("app/auth_log/$date2.log")){touch("./auth_log/$date2.log");}
+	if(file_exists("app/blocked_ip/".$ip)){
+		
+		file_put_contents("app/auth_log/$date2.log","$date [AUTH] - Authentication for $ip failed!($ip) \n", FILE_APPEND);
 		return false;
 	}
 	$password = escapeshellarg($password);
@@ -13,15 +20,18 @@ function auth($username, $password){
     	array_push($uAr1, $u2[0]);
     }
     if (!in_array($username, $uAr1)) {
+    		file_put_contents("app/auth_log/$date2.log","$date [AUTH] - Authentication for $username failed!($ip) \n", FILE_APPEND);
     		return false;
     }
 	$result = exec("sudo ./app/bin/chkpasswd $username $password");
 	if($result == "Not Authenticated"){
+		file_put_contents("app/auth_log/$date2.log","$date [AUTH] - Authentication for $username failed!($ip) \n", FILE_APPEND);
 		return false;
 	} elseif($result == "Authenticated"){
 		return true;
 	} else {
 		$_SESSION['loginAPIError'] = $result;
+		file_put_contents("app/auth_log/$date2.log","$date [AUTH] - Authentication for $username failed!($ip) \n", FILE_APPEND);
 		return false;
 	}
 }
