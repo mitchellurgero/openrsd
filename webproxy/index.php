a30968056e4ab97423175d216747f91701ef49ca
mitchellurgero <info@urgero.org>
2016-11-23 18:36:58 +0000

    Big security hole fixed Phew :/

--- webproxy/index.php
+++ webproxy/index.php
@@ -1,5 +1,8 @@
 <?php
-
+session_start();
+if(!isset($_SESSION['username'])){
+	die("You must be logged in to view this page!");
+}
 define('PROXY_START', microtime(true));
 
 require("vendor/autoload.php");
