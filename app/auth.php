839c23158b584122e4e36dbe3e5f85061e64a5c0
mitchellurgero <info@urgero.org>
2018-01-07 18:57:10 +0000

    Now we can login on arm6 \& arm7 devices by checking machine type. This will help with login for rpi0/1/2/3

--- app/auth.php
+++ app/auth.php
@@ -4,6 +4,8 @@ function auth($username, $password){
 	$date2 = date('m-d-Y', time());
 	$dir = getcwd();
 	$ip = $_SERVER['REMOTE_ADDR'];
+	$cpu = php_uname("m");
+	$chkpasswd = "chkpasswd";
 	if(!file_exists("app/auth_log/$date2.log")){touch("./auth_log/$date2.log");}
 	if(file_exists("app/blocked_ip/".$ip)){
 		
@@ -23,7 +25,11 @@ function auth($username, $password){
     		file_put_contents("app/auth_log/$date2.log","$date [AUTH] - Authentication for $username failed!($ip) \n", FILE_APPEND);
     		return false;
     }
-	$result = exec("sudo ./app/bin/chkpasswd $username $password");
+    //test if RPi0 / 1 / 2
+    if($cpu == "armv6l"){
+    	$chkpasswd = "chkpasswd6";
+    }
+	$result = exec("sudo ./app/bin/$chkpasswd $username $password");
 	if($result == "Not Authenticated"){
 		file_put_contents("app/auth_log/$date2.log","$date [AUTH] - Authentication for $username failed!($ip) \n", FILE_APPEND);
 		return false;
