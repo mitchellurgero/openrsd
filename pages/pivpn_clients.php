3364d23d809c87c06f89b2b13f6483dc696e26dc
racass <rcassiolato@outlook.com>
2019-03-13 11:31:44 -0300

    added pivpn clients

--- pages/pivpn_clients.php
+++ pages/pivpn_clients.php
@@ -39,7 +39,7 @@ if (!isset($_SESSION['username'])) {
                         {
                             if(strpos($shll_array[$i], 'CLIENT_LIST') !== false)
                             {
-                                $fin_array = preg_split(("\t", $shll_array[i]);
+                                $fin_array = preg_split("\t", $shll_array[i]);
                                 echo "<tr><td>".$fin_array[1]."</td><td>".$fin_array[2]."</td><td>".$fin_array[3]."</td><td>".$fin_array[7]."</td></tr>";
                             }
                         }
