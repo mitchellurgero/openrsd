794ae6b5f49f38912bd11ccbe6891bac77a16735
racass <racass>
2019-03-14 16:31:04 -0300

    dashboard

--- plugins/Dashboard_Clients/Dashboard_ClientsPlugin.php
+++ plugins/Dashboard_Clients/Dashboard_ClientsPlugin.php
@@ -25,6 +25,7 @@ class dashboard_ClientsPlugin extends Plugin
                     $finalStr += "<tr><td>".$fin_array[1]."</td><td>".$ip_noport[0]."</td><td>".$fin_array[3]."</td><td>".$fin_array[7]."</td></tr>";
                 }
             }
+            return $finalStr;
         }
         
         public function onPageLoadEnd($sess, &$post)
