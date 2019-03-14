6d99986f9e355e0cb8220e13150c3ffc03c1856a
racass <racass>
2019-03-14 15:08:01 -0300

    added plugins

--- plugins/Example/ExamplePlugin.php
+++ plugins/Example/ExamplePlugin.php
@@ -4,7 +4,6 @@ class ExamplePlugin extends Plugin{
 	public function __construct(){
 		//this is required by the plugin system to get working properly. This adds all the below events to global like {Class}::{onEventName};
 		parent::__construct(); //Required
-		echo "<!-- TESTE -->"
 		return true;
 	}
 	
