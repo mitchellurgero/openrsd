42ab078e17c7ee0c2c69f464c3fe196bdd419111
racass <racass>
2019-03-14 16:25:42 -0300

    added pivpn clients

--- plugins/Dashboard_Clients/Dashboard_ClientsPlugin.php
+++ plugins/Dashboard_Clients/Dashboard_ClientsPlugin.php
@@ -12,14 +12,79 @@ class dashboard_ClientsPlugin extends Plugin
         }
         public function onPageLoadEnd($sess, &$post)
         {
+            /* 
+            Panel example:
+            <div class="panel panel-default">
+                <div class="panel-heading h3">
+                    Logged in users
+                </div>
+                <div class="panel-body">
+                    <p>
+                        </p>
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        pi       tty7         Mar 13 01:51 (:0)
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        pi       pts/0        Mar 14 17:20 (192.168.1.35)
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    <p></p>
+                </div>
+                <div class="panel-footer">
+                    &nbsp;
+                </div>
+            </div>
+            */
             if($post['page'] == "dashboard")
             {    
-                echo '<div class="alert alert-success">This will be my new dashboard!</div>';
+                echo '<div class="panel panel-default">
+                <div class="panel-heading h3">
+                    Logged in users
+                </div>
+                <div class="panel-body">
+                    <p>
+                        </p>
+                        <table class="table">
+                        <tbody id="tableBody">
+                        <tr>
+                            <th>User</th>
+                            <th>IP WAN</th>
+                            <th>IP Lan</th>
+                            <th>Connect time</th>
+                        </tr>'.mountTable().'</tbody>
+                            </table>
+                        <p></p>
+                    </div>
+                    <div class="panel-footer">
+                        &nbsp;
+                    </div>
+                </div>';
             }
             echo "<!-- teste :". $post['page'] ."--> ";
             return true;
         }
-
+        private function mountTable()
+        {
+            $return = shell_exec("sudo cat /var/log/openvpn-status.log");
+            $shll_array = explode("\n", $return);
+            $finalStr = ""
+            for($i = 0; $i < count($shll_array); $i++)
+            {
+                if(strpos($shll_array[$i], 'CLIENT_LIST') !== false and strpos($shll_array[$i], 'HEADER') == false and strpos($shll_array[$i], 'Common Name') == false)
+                {
+                    $fin_array = explode("\t", $shll_array[$i]);
+                    $ip_noport = explode(":", $fin_array[2]);
+                    $finalStr += "<tr><td>".$fin_array[1]."</td><td>".$ip_noport[0]."</td><td>".$fin_array[3]."</td><td>".$fin_array[7]."</td></tr>";
+                }
+            }
+        }
         public function onPageLoad($s, &$p){
 		
             //echo '<div class="alert alert-success">This should be displayed via the Exmaple Plugin!</div>';
