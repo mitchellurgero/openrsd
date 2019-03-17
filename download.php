54896b43215c6e3de1bf2ecf1e4ce56b0e36a19b
Mitchell Urgero <info@urgero.org>
2019-03-17 21:12:58 +0000

    Fixed HUGE security bug in downloader scripts

--- download.php
+++ download.php
@@ -3,11 +3,17 @@ if (!isset($_SESSION)) {
     session_start();
 };
 if (isset($_SESSION['username']) && isset($_GET['filename'])) {
-    $file = './app/scr_up/'.$_GET['filename'];
-    header('Content-type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.$_GET['filename'].'"');
-    header('Content-length: ' . filesize($file));
-    readfile($file);
+    $filename = str_replace(array("..","/"),array("",""),$_GET['filename']);
+    $file = './app/scr_up/'.$filename;
+    if(file_exists($file)){
+        header('Content-type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.$filename.'"');
+        header('Content-length: ' . filesize($file));
+        readfile($file);
+    } else {
+        die("Invalid filename!");
+    }
+
 } else {
     header("Location: ./index.php");
 }
